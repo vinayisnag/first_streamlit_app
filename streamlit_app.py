f09f7508c5e837d25c20b7654c34c851b7ca7ecd
vinayisnag <117552203+vinayisnag@users.noreply.github.com>
2022-11-05 19:21:18 -0700

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('OMega 3 and Blueberry OatMeal')
-Streamlit.text('Idly and Dosa with Samber and Chutney')
-Streamlit.text('Pancake with Maple Syrup')
+streamlit.text('Idly and Dosa with Samber and Chutney')
+streamlit.text('Pancake with Maple Syrup')
