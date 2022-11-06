580be48bf9773d78f4acba6acd7732cda4c30532
vinayisnag <117552203+vinayisnag@users.noreply.github.com>
2022-11-05 19:19:25 -0700

    Update streamlit_app.py
    Space

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('OMega 3 & Blueberry OatMeal')
 Streamlit.text('Idly & Dosa with Samber and Chutney')
