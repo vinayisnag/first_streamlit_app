87c29368fb0e3afd741333f1281500b56b135903
vinayisnag <117552203+vinayisnag@users.noreply.github.com>
2022-11-05 19:28:54 -0700

    Update streamlit_app.py
    Added fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,7 @@ streamlit.text(' OMega 3 and Blueberry OatMeal')
 streamlit.text('🍌🥭 Idly and Dosa with Samber and Chutney')
 streamlit.text('Pancake with Maple Syrup')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
