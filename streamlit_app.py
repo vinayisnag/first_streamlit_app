5ec69211c67f43ef765cae40e63634103d52dfe2
vinayisnag <117552203+vinayisnag@users.noreply.github.com>
2022-11-05 19:16:18 -0700

    Update streamlit_app.py
    Added few healthy breakfast items Values

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('OMega 3 & Blueberry OatMeal')
+Streamlit.text('Idly & Dosa with Samber and Chutney')
+Streamlit.text('Pancake with Maple Syrup')
