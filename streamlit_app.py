8a74f528e4806b73d2725f85fd3d0ffcc96260a3
vinayisnag <117552203+vinayisnag@users.noreply.github.com>
2022-11-05 19:20:01 -0700

    Update streamlit_app.py
    descripton of menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('OMega 3 & Blueberry OatMeal')
-Streamlit.text('Idly & Dosa with Samber and Chutney')
+streamlit.text('OMega 3 and Blueberry OatMeal')
+Streamlit.text('Idly and Dosa with Samber and Chutney')
 Streamlit.text('Pancake with Maple Syrup')
